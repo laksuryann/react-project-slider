72ab970307b2c72eda3721dbc4ddf4ba4efdd5af
venkatesh dharmavarapu <venkatadharma1@gmail.com>
2021-03-24 20:06:21 +0530

    whole new dat is creatde for this new slider

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const people = [
+    {
+      id: 1,
+      image:
+        'https://scontent.fhyd1-4.fna.fbcdn.net/v/t1.0-9/122330637_2770654983176795_7604211432092175115_o.jpg?_nc_cat=102&ccb=1-3&_nc_sid=174925&_nc_ohc=AtEmtapEOq8AX96t1RH&_nc_ht=scontent.fhyd1-4.fna&oh=c42104cc8d1c9e586fa0297d50669c55&oe=607CF68B',
+      name: 'Tarun Spartan',
+      title: 'office manager',
+      quote:
+        'hey there .. i am office manager in this company ... my colleaques are so cool .. team leads and other senior developers are so encouraging ... ',
+    },
+    {
+      id: 2,
+      image:
+        'https://scontent.fhyd1-2.fna.fbcdn.net/v/t1.0-9/141010418_2591093811036211_2772754920310658966_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=Ein6rtHYkn4AX9F5T4T&_nc_ht=scontent.fhyd1-2.fna&oh=6c283987c4a5da954587224d1636aa58&oe=607A7957',
+      name: 'sushmitha chari',
+      title: 'product designer',
+      quote:
+        'i recently joined into the team & there i am receiving lot of supporting from juniors to seniors as well this ... was sooo amazing to say na ...',
+    },
+    {
+      id: 3,
+      image:
+        'https://scontent.fhyd1-2.fna.fbcdn.net/v/t1.0-9/89682083_2610006975911385_4519953114781974528_o.jpg?_nc_cat=107&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=LVB7Yf5V9YIAX_6fY11&_nc_ht=scontent.fhyd1-2.fna&oh=5567baaf2b143982ed5a08b699032b8f&oe=607CFDC4',
+      name: 'sri Harha',
+      title: 'regular guy',
+      quote:
+        'came from a big company to here ... no mention for the previous company ... but a beautiful & supportive stafff here makes me more warmer ...',
+    },
+    {
+      id: 4,
+      image:
+        'https://scontent.fhyd1-3.fna.fbcdn.net/v/t1.0-9/89654847_2549720435137986_6155859523073474560_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=730e14&_nc_ohc=hl38X69uHfEAX-vN5tE&_nc_ht=scontent.fhyd1-3.fna&oh=b591becb9210ba5fe3e799312e1692bd&oe=607B90A1',
+      name: 'mano raja venky',
+      title: 'the boss Gang',
+      quote:
+        ' Do enter into the company ... you can simply cant believe that we are the bosses of this five hundred fortune company ... ',
+    },
+  ];
+  
+  export default people;
+  
